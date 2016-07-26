74dfb1268b66d876642fe02a36558888a14ffbb0
zhangmh <zhangmhao@gmail.com>
2016-07-26 17:12:23 +0800

    feat: import words

--- saveToDatabase.js
+++ saveToDatabase.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const conn = require('./conn');
 const PythonShell = require('python-shell');
 const recursive = require('recursive-readdir');
@@ -8,38 +9,47 @@ const LIB_FOLDER = path.resolve(__dirname, './download');
 const Word = require('./model/Word');
 const Category = require('./model/Category');
 const Dictionary = require('./model/Dictionary');
+const readline = require('readline');
 // connect to database
 conn.connect();
 
 
-recursive(LIB_FOLDER, function (err, files) {
-  // Files is an array of filename
-  files.reduce((promise, file) => {
-    return promise.then(() => {
-      if (/\.scel$/.test(file)) {
-        return importFile(file);
-      } else {
-        return Promise.resolve();
-      }
-    });
-  }, Promise.resolve(1)).then(() => {
-    console.log('done');
-  }, err => {
-    console.log(err);
-    conn.close();
-  });
-});
+// recursive(LIB_FOLDER, function (err, files) {
+//   // Files is an array of filename
+//   files.reduce((promise, file) => {
+//     return promise.then(() => {
+//       if (/\.scel$/.test(file)) {
+//         return importFile(file);
+//       } else {
+//         return Promise.resolve();
+//       }
+//     });
+//   }, Promise.resolve(1)).then(() => {
+//     console.log('done');
+//     process.exit(0);
+//   }, err => {
+//     console.log(err);
+//     conn.close();
+//     process.exit(0);
+//   });
+// });
 
 function importFile(file) {
   const fileInfoArr = file.split('/');
   const cateName = fileInfoArr[fileInfoArr.length - 2];
   const fileName = fileInfoArr[fileInfoArr.length - 1];
   const dictName = fileName.slice(0, fileName.lastIndexOf('.'));
-  console.log(`正在入库 ${cateName} : ${dictName} ...`);
+
   return importCategory(cateName)
     .then(cate => {
       return importDictionary(dictName, cate._id);
-    }, err => {
+    }).then(() => {
+      return parseScelFile();
+    }).then(() => {
+      return importTxt();
+    }).then(() => {
+      console.log(`正在入库 ${cateName} : ${dictName} 成功`);
+    }, (err) => {
       console.log(err);
       throw err;
     });
@@ -103,38 +113,58 @@ function importDictionary(dictName, cate) {
 }
 
 
+function importWord(word, source, cateId, dictId) {
+  return new Promise((resolve, reject) => {
+    Word.create({
+      source: source,
+      text: word.text, // 单词文字
+      cate: cateId, // 分类
+      dict: dictId, // 字典名称
+      py: word.py, // 拼音
+      createTime: new Date(),
+      lastEditTime: new Date(),
+    }, (err, word) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(word);
+      }
+    });
+  });
+}
 
+function importTxt() {
+  let lines = [];
+  let lineReader = readline.createInterface({
+    input: fs.createReadStream('./sougou.txt')
+  });
 
+  lineReader.on('line', function (line) {
+    const word = getWord(line);
+    lineReader.pause();
+    importWord(word).then( () => {
+      lineReader.resume();
+    }, (err) => {
+      console.log(err);
+      throw err;
+    });
+  });
 
+  lineReader.on('close', function() {
+    resolve();
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var options = {
-//   mode: 'text',
-//   pythonPath: 'path/to/python',
-//   pythonOptions: ['-u'],
-//   scriptPath: 'path/to/my/scripts',
-//   args: ['value1', 'value2', 'value3']
-// };
-//
-// PythonShell.run('my_script.py', options, function (err, results) {
-//   if (err) throw err;
-//   // results is an array consisting of messages collected during execution
-//   console.log('results: %j', results);
-// });
+function parseScelFile(filePath) {
+  return new Promsie((resolve, reject) => {
+    var options = {
+      args: [filePath]
+    };
+    PythonShell.run('toTxt.py', options, function (err, results) {
+      if (err) {
+        return reject (err);
+      }
+      resolve(results);
+    });
+  });
+}
