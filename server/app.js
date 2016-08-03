6cf4bca3c531809427a5933892874e59d2a9f378
zhangmh <zhangmhao@gmail.com>
2016-08-03 23:24:27 +0800

    feat: 添加categories接口

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const _s = require('strman');
 const BusinessError = require('./error/BusinessError.js');
 const DEFAULT_SNAKE_SIZE = 10;
 const DEFAULT_ASSOCIATE_SIZE = 1;
+const QUERRY_SPLITER = /,/;
 // connect to database
 conn.connect();
 
@@ -22,13 +23,27 @@ router.get('/associate', function*(next) {
   const word = query.word;
   let size = query.size;
   size = size ? parseInt(size, 10) : DEFAULT_ASSOCIATE_SIZE;
+  const categories = parseQueryToArray(query.categories);
+  const dictionaries = parseQueryToArray(query.dictionaries);
   if (!word) {
     throw new BusinessError('args_required', 'need word params');
   }
   const lastChar = _s.last(word, 1);
-  let words = yield randomWord({
+  const filter = {
     text: new RegExp('^' + lastChar),
-  }, size);
+  };
+  if (categories) {
+    filter.cate = {
+      $in: categories,
+    };
+  }
+  if (dictionaries) {
+    filter.dict = {
+      $in: dictionaries,
+    };
+  }
+  console.log(filter);
+  let words = yield randomWord(filter, size);
   if (size === 1) {
     words = [words];
   }
@@ -45,6 +60,8 @@ router.get('/snake', function*(next) {
   let word = query.word;
   let size = query.size;
   size = size ? parseInt(size, 10) : DEFAULT_SNAKE_SIZE;
+  const categories = parseQueryToArray(query.categories);
+  const dictionaries = parseQueryToArray(query.dictionaries);
   if (!word) {
     word = yield randomWord();
     word = word.text;
@@ -54,13 +71,33 @@ router.get('/snake', function*(next) {
   let index = 1;
   while(index++ < size) {
     const lastChar = _s.last(curWord, 1);
-    const newWord = yield randomWord({
+    const filter = {
       text: new RegExp('^' + lastChar),
-    });
+    };
+    if (categories) {
+      filter.cate = {
+        $in: categories,
+      };
+    }
+    if (dictionaries) {
+      filter.dict = {
+        $in: dictionaries,
+      };
+    }
+    const newWord = yield randomWord(filter, 1);
     curWord = newWord.text;
     words.push(curWord);
   }
   this.body = words;
+});
+
+
+/**
+ * 获取词库分类
+ */
+router.get('/categories', function*(next) {
+  const categories = yield Category.find().exec();
+  this.body = categories;
 });
 
 function randomWord(filter, size) {
@@ -81,6 +118,10 @@ function randomWord(filter, size) {
       }
     });
   });
+}
+
+function parseQueryToArray(str) {
+  return typeof str === 'string' ? str.split(QUERRY_SPLITER) : null;
 }
 
 app.use(function*(next) {
